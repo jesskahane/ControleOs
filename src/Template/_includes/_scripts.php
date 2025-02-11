7e27b46e7f5e15d31668083b4711b3b9aa897250
jesskahane <jesskt.contato@gmail.com>
2025-02-10 23:58:40 -0300

    Initial commit

---
+++ src/Template/_includes/_scripts.php
@@ -0,0 +1,8 @@
+<!-- jQuery -->
+<script src="../../Template/plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="../../Template/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="../../Template/dist/js/adminlte.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="../../Template/dist/js/demo.js"></script>
